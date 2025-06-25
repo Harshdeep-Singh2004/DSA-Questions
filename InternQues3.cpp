6c5a55ebcd9328629015c95c335157c815b61a73
Harshdeep-Singh2004 <143889275+Harshdeep-Singh2004@users.noreply.github.com>
2025-06-25 21:42:05 +0530

    Create InternQues3.cpp

---
+++ InternQues3.cpp
@@ -0,0 +1,4 @@
+// Q. Imagine a terrain having different height on different points. Some watch towers are located on the terrain which emit light towards their left and right. Your job is to compute which place is illuminated and which is not. 
+//   You are given an array of N integers and another array of K integers from range [0 … N-1]. The first array describes the terrain - these are heights of the points. 
+//   The second array contains positions of towers on the terrain. towers are sorted in ascending order by their positions. 
+//   Compute a bit mask of length N (an array of integers or booleans) that will encode 1 (or true) on each position illuminated by the tower.
